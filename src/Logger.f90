105b80115f71c8e08be5d4aded417302a1725396
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-12-11 15:30:58 -0500

    Replacing old classes

--- src/Logger.f90
+++ src/Logger.f90
@@ -1,56 +1,45 @@
-module PrivateLogger_mod
+module ASTG_Logger_mod
+  use ASTG_FileHandler_mod
+
+
    implicit none
    private
 
-   public Logger
-   public newLogger
-   public LoggerManager
-   public newLoggerManager
+   public :: Logger
+   public :: newLogger
 
-   type Logger
-      integer :: level
+   type :: Logger
+     character(len=128) :: name
+     type(FileHandler) :: handler
+   contains
+     procedure :: log_integer
+     generic :: log => log_integer
    end type Logger
 
-   type LoggerManager
-      type(Logger), allocatable :: Loggers(:)
-   end type LoggerManager
-
    interface newLogger
-      module procedure Logger_
-   end interface newLogger
-
+     module procedure newFileLogger
+   end interface
+   
 contains
 
-   type(Logger) function Logger_()
-      Logger_%level = 0
-   end function Logger_ 
+  function newFileLogger(name) result(log)
+    character(len=*), intent(in) :: name
+    type (Logger) :: log
+    log%handler = newFileHandler(name)
+    log%name = name
+  end function newFileLogger
 
-   function newLoggerManager() result(list)
-      type (LoggerManager) :: list
-      allocate(list%loggers(0))
-   end function newLoggerManager
+  subroutine log_integer(this, i)
+    class (Logger), intent(inout) :: this
+    integer, intent(in) :: i
+    
+    if (.not. this%handler%isOpen()) call this%handler%open()
+    write(this%handler%getUnit(),'(a)') this%handler%toString(i)
+    call flush(this%handler%getUnit())
 
-end module PrivateLogger_mod
+   end subroutine log_integer
 
-module Logger_mod
-   use PrivateLogger_mod
-   implicit none
-   private
-
-   public Logger
-   public newLogger
-   public LoggerManager
-   public newLoggerManager
-   public :: initializeGlobalLoggerManager
-
-   type (LoggerManager) :: globalLoggerManager ! private
-
-contains
-
-   subroutine initializeGlobalLoggerManager()
-      globalLoggerManager = newLoggerManager()
-   end subroutine initializeGlobalLoggerManager
-
-end module Logger_mod
+  
+ end module ASTG_Logger_mod
 
 
