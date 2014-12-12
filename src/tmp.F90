d1e740801168ad36b69547f1a04ee24cfc91e288
Tom <Thomas.L.Clune@nasa.gov>
2014-12-12 17:35:18 -0500

    Fixed capitalization in file name for Logger.f90 => Logger.F90

---
+++ src/tmp.F90
@@ -0,0 +1,89 @@
+module ASTG_Logger_mod
+   use ASTG_AbstractHandler_mod
+   use FTL_AbstracthandlerPolyWrap_mod
+   use FTL_AbstracthandlerPolyWrapVector_mod
+
+   implicit none
+   private
+
+   public :: Logger
+
+   type :: Logger
+      private
+      integer :: level
+      type (AbstractHandlerPolyWrapVector) :: handlers
+   contains
+      procedure :: log
+      procedure :: addHandler
+      procedure :: removeHandler
+      procedure :: getHandlers
+   end type Logger
+
+   interface Logger
+      module procedure newLogger
+   end interface Logger
+
+contains
+
+   
+   function newLogger() result(alog)
+      use ASTG_DefaultHandler_mod
+      type (Logger) :: alog
+
+! TODO: Need to NOTSET when inheritance is working
+      alog%level = INFO
+      alog%handlers = AbstractHandlerPolyWrapVector()
+      call alog%addHandler(DefaultHandler())
+      
+   end function newLogger
+
+   
+   subroutine addHandler(this, handler)
+      class (Logger), intent(inout) :: this
+      class (AbstractHandler), intent(in) :: handler
+      
+      call this%handlers%push_back(AbstractHandlerPolyWrap(handler))
+      
+   end subroutine addHandler
+
+
+   subroutine removeHandler(this, handler)
+      class (Logger), intent(inout) :: this
+      class (AbstractHandler), intent(in) :: handler
+
+      call this%handlers%pop_back()
+      
+   end subroutine removeHandler
+
+
+   subroutine log(this, level, message)
+      class (Logger), intent(inout) :: this
+      integer, intent(in) :: level
+      character(len=*), intent(in) :: message
+      
+      type (AbstractHandlerPolyWrapVectorIterator) :: iter
+      type (AbstractHandlerPolyWrap), pointer :: handlerWrap
+      class (AbstractHandler), pointer :: handler
+
+      iter = this%handlers%begin()
+      do while (iter /= this%handlers%end())
+         handlerWrap => iter%get()
+         handler => handlerWrap%get()
+         call handler%emit(level, message)
+         call iter%next()
+      end do
+
+   end subroutine log
+
+
+   function getHandlers(this) result(handlers)
+      class (Logger), target, intent(in) :: this
+      type (AbstractHandlerPolyWrapVector), pointer :: handlers
+      
+      handlers => this%handlers
+      
+   end function getHandlers
+
+end module ASTG_Logger_mod
+
+
