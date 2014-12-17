84c02de5287f59779cfef5416bfa4f02dafe29a3
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-12-17 09:36:08 -0500

    first implementation of rotating file handler

--- src/RotatingFileHandler.F90
+++ src/RotatingFileHandler.F90
@@ -19,13 +19,17 @@ module ASTG_RotatingFileHandler_mod
    contains
       procedure :: doRollover
       procedure :: shouldRollover
+      procedure :: emitMessage
    end type RotatingFileHandler
 
    interface RotatingFileHandler
       module procedure newRotatingFileHandler
    end interface
 
-
+   integer, parameter :: TEST_MAX_NUM_RECORDS = 10
+   integer, parameter :: MAX_NUM_RECORDS = 1000000 ! what is a good number?
+   integer, save :: backupCounter =0
+   
 contains
 
     
@@ -37,42 +41,97 @@ contains
       integer, intent(in), optional :: level
       integer(int64), intent(in), optional :: maxBytes
       integer, intent(in), optional :: backupCount
+      
       integer :: level_
+      integer :: maxBytes_
+      integer :: backupCount_
 
       if (present(level)) then
         level_ = level
       else
         level_ = INFO
       end if
+      if (present(maxBytes)) then
+        maxBytes_ = maxBytes
+      else
+        maxBytes_ = 0
+      end if
+      if (present(backupCount)) then
+        backupCount_ = backupCount
+      else
+        backupCount_ = 0
+      end if
       
       call handler%setFileName(fileName)
       call handler%open()
       call handler%setLevel(level_)
-      handler%maxBytes = maxBytes
-      handler%backupCount = backupCount
+      handler%maxBytes = maxBytes_
+      handler%backupCount = backupCount_
+      handler%numRecords = 0
       
    end function newRotatingFileHandler
 
+   
+   subroutine emitMessage(this, levelString, message)
+      ! Write a string to a file. Level is specified in levelString
+      class (RotatingFileHandler), intent(inout) :: this
+      character(len=*), intent(in) :: levelString
+      character(len=*), intent(in) :: message
+
+      if (this%shouldRollover()) then
+         call this%doRollover()
+      else
+         !write(this%unit,'(a)') levelString // ': ' // message
+      end if
     
-   logical function shouldRollover(this)
+   end subroutine emitMessage
+
+        
+   function shouldRollover(this) result(rollOver)
       ! Determine if rollover should occur.
       class (RotatingFileHandler), intent(in) :: this
-
-      ! if maxBytes exceeded then...
+      logical :: rollOver 
+      integer :: fileSize
       
+      rollOver = .false.
+      inquire(this%getUnit(), SIZE=fileSize)
+      if (fileSize > this%maxBytes) then
+         rollOver = .true.
+      end if
+        
    end function shouldRollover
 
 
    subroutine doRollover(this)
       ! Rollover occurs whenever the current log file is nearly maxBytes
       class (RotatingFileHandler), intent(inout) :: this
-
-      ! if backupCount > 0
-      !   rename old filename
-      !   create new filename (use basename?)
-      !   backupCount--
+      character(len=:), allocatable :: suffix
+      
+      if ((this%backupCount) > 0) then
+         write(suffix,'(a)') backupCounter+1
+         call execute_command_line('mv '//this%getFileName()// &
+              ' '//this%getFileName()//'.'//suffix)
+         this%backupCount = this%backupCount - 1
+         call execute_command_line('touch '//this%getFileName())
+       end if
       
    end subroutine doRollover
+
+
+   subroutine doRolloverNR(this)
+      ! Rollover occurs whenever the current log file exceeds number of records
+      class (RotatingFileHandler), intent(inout) :: this
+      character(len=:), allocatable :: suffix
+      
+      if ((this%numRecords) > TEST_MAX_NUM_RECORDS) then
+         write(suffix,'(a)') this%backupCount
+         call execute_command_line('mv '//this%getFileName()// &
+              ' '//this%getFileName()//'.'//suffix)
+         this%numRecords = this%numRecords - 1
+         call execute_command_line('touch '//this%getFileName())
+       end if
+      
+   end subroutine doRolloverNR
 
    
 end module ASTG_RotatingFileHandler_mod
