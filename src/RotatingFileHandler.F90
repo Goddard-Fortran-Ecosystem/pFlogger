066c28fca5ca3ddd528ab99c467b002d40fbbc14
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-12-17 21:37:07 -0500

    Change maxBytes argument to be a string. This differs from python design but it is more flexible as one can specify maxBytes='30mb' rather than maxBytes=30000000 if we keep maxBytes as an integer.

--- src/RotatingFileHandler.F90
+++ src/RotatingFileHandler.F90
@@ -4,6 +4,7 @@ module ASTG_RotatingFileHandler_mod
    ! By default, the file grows indefinitely. You can specify particular
    ! values of maxBytes and backupCount to allow the file to rollover at
    ! a predetermined size.
+   use iso_fortran_env
    use ASTG_SeverityLevels_mod, only: INFO
    use ASTG_FileHandler_mod, only: FileHandler
    implicit none
@@ -13,7 +14,7 @@ module ASTG_RotatingFileHandler_mod
 
    type, extends(FileHandler) :: RotatingFileHandler
       private
-      integer :: maxBytes
+      integer(int64) :: maxBytes
       integer :: backupCount
    contains
       procedure :: doRollover
@@ -25,39 +26,39 @@ module ASTG_RotatingFileHandler_mod
       module procedure newRotatingFileHandler
    end interface
 
-   integer, save :: backupCounter
+   integer, save :: fileCount
+
    
 contains
 
     
-   function newRotatingFileHandler(fileName, maxBytes, backupCount, level) &
+   function newRotatingFileHandler(fileName, numBytes, backupCount, level) &
         result(handler)
-      use iso_fortran_env
       ! Initializes the instance
       type (RotatingFileHandler) :: handler
       character(len=*), intent(in) :: fileName
-      integer(int64), intent(in), optional :: maxBytes
+      character(len=*), intent(in), optional :: numBytes
       integer, intent(in), optional :: backupCount
       integer, intent(in), optional :: level
       
       integer :: level_
-      integer :: maxBytes_
       integer :: backupCount_
+      integer(int64) :: maxBytes_
 
       if (present(level)) then
-        level_ = level
+         level_ = level
       else
-        level_ = INFO
+         level_ = INFO
       end if
-      if (present(maxBytes)) then
-        maxBytes_ = maxBytes
+      if (present(numBytes)) then
+         maxBytes_ = convertNumBytes_(numBytes)
       else
-        maxBytes_ = 0
+         maxBytes_ = 0
       end if
       if (present(backupCount)) then
-        backupCount_ = backupCount
+         backupCount_ = backupCount
       else
-        backupCount_ = 0
+         backupCount_ = 0
       end if
       
       call handler%setFileName(fileName)
@@ -65,10 +66,52 @@ contains
       call handler%setLevel(level_)
       handler%maxBytes = maxBytes_
       handler%backupCount = backupCount_
-      backupCounter = 0
+      fileCount = 0
       
    end function newRotatingFileHandler
 
+   
+   function convertNumBytes_(numBytes) result(nBytes)
+      character(len=*), intent(in) :: numBytes
+      integer(int64) :: nBytes
+      
+      character(len=16) :: fmt
+      integer :: pos, n, nl
+
+      pos = scan(numBytes,'kmg')      
+      if (pos > 0) then
+        nl = len(numBytes(1:pos-1))        
+        fmt = setFmt_(nl)
+        select case(numBytes(pos:pos+1))
+        case ('kb')
+          read(numBytes(1:pos-1), fmt) n
+          nBytes = n * 1E3
+        case ('mb')
+          read(numBytes(1:pos-1), fmt) n
+          nBytes = n * 1E6
+        case ('gb')
+          read(numBytes(1:pos-1), fmt) n
+          nBytes = n * 1E9
+        end select
+      else
+        nl = len(numBytes)
+        fmt = setFmt_(nl)
+        read(numBytes, fmt) n
+        nBytes = n
+      end if
+
+   contains
+      
+      function setFmt_(n) result (fmt)
+         integer, intent(inout) :: n
+         character(len=1) :: fmt0
+         character(len=4) :: fmt
+         write(fmt0,'(i1)')n
+         fmt =  '(i'//trim(fmt0)//')'
+      end function setFmt_
+      
+   end function convertNumBytes_
+      
    
    subroutine emitMessage(this, levelString, message)
       ! Write a string to a file. Level is specified in levelString
@@ -101,18 +144,30 @@ contains
 
    
    subroutine doRollover(this)
-      ! Rollover occurs whenever the current log file is nearly maxBytes
+      ! Rollover occurs whenever the current log file exceeds maxBytes
       class (RotatingFileHandler), intent(inout) :: this
+      
       character(len=64) :: suffix
       character(len=128) :: cmd
+      character(len=6) :: fmt
       logical :: is
 
       if (this%isOpen()) then
          call this%close()
       end if
-      if (backupCounter < this%backupCount) then
-         backupCounter = backupCounter + 1
-         write(suffix,'(i1.1)') backupCounter
+      if (fileCount < this%backupCount) then
+         fileCount = fileCount + 1
+         select case (fileCount)
+         case (:9)
+           fmt = '(i1.1)'
+         case (10:99)
+           fmt = '(i2.2)'
+         case (100:999)
+           fmt = '(i3.3)'
+         case (1000:9999)
+           fmt = '(i4.4)'
+         end select
+         write(suffix, fmt) fileCount
          ! sanity check
          inquire(FILE=this%getFileName(), EXIST=is)
          if (is) then ! rename file
