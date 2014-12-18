f1f4564445dfd406e6c813679889fcdeef344ee2
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-12-18 09:43:25 -0500

    minor update

--- app/app.F90
+++ app/app.F90
@@ -1,6 +1,17 @@
 program app
-   use Logger_mod
+   use ASTG_Logger_mod
+   use ASTG_StreamHandler_mod
+   use ASTG_SeverityLevels_mod
+   implicit none
+   
    type (Logger) :: log
+   type (StreamHandler) :: sh
+   
    print *,'Create logger'
-   log = newLogger()
+   log = Logger()
+   call sh%setLevel(WARNING)
+   call log%addHandler(sh)
+
+   call log%warning('Hello world')
+      
 end program app
