3a08f14d07ebf83b6fce94b902ce0c6594128072
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-12-30 15:24:13 -0500

    added token

--- src/FormatParser.F90
+++ src/FormatParser.F90
@@ -3,11 +3,19 @@ module ASTG_FormatParser_mod
    private
 
    public FormatParser
+   public Token
    
    type FormatParser
    contains
       procedure, nopass :: isFormat
+      procedure, nopass :: getTokens
    end type FormatParser
+
+   type Token
+      character(len=:), allocatable :: token
+   end type Token
+   
+   character(len=1), parameter :: FORMAT_DELIMITER = '%'
    
 contains
 
@@ -15,8 +23,16 @@ contains
    logical function isFormat(string)
       character(len=*) :: string
 
-      isFormat = (string(1:1)=='%')
+      isFormat = (string(1:1) == FORMAT_DELIMITER)
       
    end function isFormat
 
+   function getTokens(string) result(tokens)
+      type(Token), allocatable :: tokens(:)
+      character(len=*) :: string
+
+      allocate(tokens(0))
+      
+   end function getTokens
+   
 end module ASTG_FormatParser_mod
