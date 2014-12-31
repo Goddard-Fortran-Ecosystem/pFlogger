1b0ed349d571dd96cdb70407916f21b8664f01eb
Tom <Thomas.L.Clune@nasa.gov>
2014-12-30 22:19:11 -0500

    Modified build so that FTL is not directly included in distribution. This will simplify the process of creating additional template instantiations.

--- src/FTL/CIString.F90
+++
@@ -1,9 +0,0 @@
-module FTL_CIString_mod
-   use FTL_CaseInsensitiveString_mod, only: CIString => CaseInsensitiveString
-   use FTL_CaseInsensitiveString_mod, only: toString
-   private
-   
-   public :: CIString
-   public :: toString
-
-end module FTL_CIString_mod
