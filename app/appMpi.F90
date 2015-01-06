f924ceca466d4b054c1c266836df2267b0f7f4ef
Carlos Cruz <carlos.a.cruz@nasa.gov>
2015-01-06 18:50:23 -0500

    Add MPI option to build run tests and apps.

---
+++ app/appMpi.F90
@@ -0,0 +1,29 @@
+program appMpi
+   use ASTG_LoggerManager_mod
+   use ASTG_MpiFileHandler_mod
+   use ASTG_Logger_mod
+   implicit none
+
+   include 'mpif.h'
+
+   type (LoggerManager) :: manager
+   class (Logger), pointer :: lgr
+   type (MpiFileHandler) :: handler
+
+   integer :: ier
+
+   call mpi_init(ier)
+
+
+   manager = LoggerManager()
+   lgr => manager%getLogger('A')
+   handler = MpiFileHandler('log', MPI_COMM_WORLD)
+   call lgr%addHandler(handler)
+
+
+   call lgr%warning('hello')
+
+   call mpi_finalize(ier)
+
+end program appMpi
+
