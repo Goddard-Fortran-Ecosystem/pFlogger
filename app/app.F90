239698cdeec299bc60d94448446357996975f825
Carlos Cruz <carlos.a.cruz@nasa.gov>
2015-01-21 13:35:58 -0500

    a modelE-like app

--- app/app.F90
+++ app/app.F90
@@ -1,36 +1,58 @@
 program fileLogging
+
+   use ASTG_LoggerManager_mod
    use ASTG_Logger_mod
+   use ASTG_Filter_mod
+   use ASTG_MpiFilter_mod
+   use ASTG_StreamHandler_mod
    use ASTG_FileHandler_mod
+   use ASTG_MpiFileHandler_mod
    use ASTG_Formatter_mod
    use ASTG_SeverityLevels_mod
+   use ASTG_SeverityLevels_mod
+   use iso_fortran_env, only: OUTPUT_UNIT
    implicit none
-   
-   type (Logger) :: log
-   type (FileHandler) :: fHandler
-   type(Formatter) :: fmt
-   
-   ! Create a Logger and give it a name.
-   ! NOTE: Loggers have message levels to filter out messages
-   ! and default is INFO.
-   log = Logger('appLog')
 
-   ! Create a file handler, argument is fileName
-   fHandler = FileHandler('app.LOG')
-   ! By default handler level is INFO, change to DEBUG:
-   call fHandler%setLevel(DEBUG)
+   include 'mpif.h'
+   integer :: ier
+   type (MpiFilter) :: rootFilter
+   type (Filter) :: debugFilter
+   type (FileHandler) :: errFile
+   type (StreamHandler) :: prtFile
+   type (MpiFileHandler) :: debugFile
+   type (LoggerManager) :: manager
+   class (Logger), pointer :: loggerPtr
 
-   ! Create a logging format
-   fmt = Formatter('%(name:a) %(message:a)')
+   character(len=:), allocatable :: rundeck
 
-   call fHandler%setFormatter(fmt)
+   call mpi_init(ier)
+
+   rundeck = 'em20'
    
-   ! Add this handler to logger so that logger can use it
-   call log%addHandler(fHandler)
+   manager = LoggerManager()
+   rootFilter = MpiFilter(MPI_COMM_WORLD)
+
+   prtFile = StreamHandler(OUTPUT_UNIT, level=INFO)
+   call prtFile%addFilter(rootFilter)
    
-   ! Start logging!
-   call log%info('Starting fileLogging demo')
-   
-   ! Done
-   call log%info('DONE')
-   
+   errFile = FileHandler(rundeck // '.ERR', level=WARNING)
+   call errFile%addFilter(rootFilter)
+   call errFile%setFormatter(Formatter('%(rank) %(level) %(line) %(file) %a'))
+
+   loggerPtr => manager%getLogger('modelE')
+   call loggerPtr%addHandler(prtFile)
+   call loggerPtr%addHandler(errFile)
+
+   debugFilter = Filter('chemistry')
+   debugFile = MpiFileHandler('debugChem.%i3.3', MPI_COMM_WORLD, &
+        level=DEBUG, &
+        delay=.true.)
+   call debugFile%addFilter(Filter('chemistry'))
+
+   loggerPtr => manager%getLogger('modelE.chemistry')
+   call loggerPtr%addHandler(debugFile)
+
+   call mpi_finalize(ier)
+
+
 end program fileLogging
