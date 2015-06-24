0ea335ac391c38f65c4755a2a38d65993530e900
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2015-06-24 15:25:45 -0400

    ! add example

---
+++ examples/mpiLogging/simpleMPI.F90
@@ -0,0 +1,32 @@
+program simpleMpi
+   use ASTG_LoggerManager_mod
+   use ASTG_MpiFileHandler_mod
+   use ASTG_Logger_mod
+   implicit none
+
+   include 'mpif.h'
+
+   type (LoggerManager) :: manager
+   class (Logger), pointer :: lgr
+   type (MpiFileHandler) :: handler
+
+   integer :: ier
+
+   ! each rank will open and write to xxx.pe=rank file
+
+   call mpi_init(ier)
+
+   ! instantiate manager with the root node of the logger hierarchy.
+   manager = LoggerManager()
+   lgr => manager%getLogger('A')
+
+   ! each rank will open and write to log.pe=rank file
+   ! for example, filess, log.pe=0,log.pe=1, ... 
+   handler = MpiFileHandler('log', MPI_COMM_WORLD)
+   call lgr%addHandler(handler)
+
+   call lgr%warning('hello')
+   call mpi_finalize(ier)
+
+end program simpleMpi
+
