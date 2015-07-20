498f1e7bb79247e68a5f19194102d200f34a083b
Carlos Cruz <carlos.a.cruz@nasa.gov>
2015-07-20 12:20:57 -0400

    added a few more gcc debugging flags

--- cmake/modules/setCompilerFlags.cmake
+++ cmake/modules/setCompilerFlags.cmake
@@ -35,7 +35,7 @@ elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL GNU)
 
    set(CPPFLAGS "${CPPFLAGS} -cpp")
    set (CMAKE_Fortran_FLAGS_RELEASE "${CPPFLAGS} -O3")
-   set (CMAKE_Fortran_FLAGS_DEBUG "${CPPFLAGS} -O0 -g -fbacktrace -fcheck=pointer -fcheck=mem -fcheck=bounds")
+   set (CMAKE_Fortran_FLAGS_DEBUG "${CPPFLAGS} -O0 -g -fbacktrace -fcheck=pointer -fcheck=mem -fcheck=bounds -ffpe-trap=invalid,zero,overflow -finit-real=snan")
 
 # NAG compiler flags
 elseif(${CMAKE_Fortran_COMPILER_ID} STREQUAL NAG)
