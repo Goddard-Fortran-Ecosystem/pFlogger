be4adc3358a30ae1b60c9f8e178bb196d753bdc1
Tom <Thomas.L.Clune@nasa.gov>
2015-11-07 12:47:27 -0500

    Added another benchmark.

--- benchmarks/basic.F90
+++ benchmarks/basic.F90
@@ -1,9 +1,10 @@
 program main
    use pflogger_mod
+   use timer_mod
    implicit none
 
-   integer :: log_n
-   integer :: n
+   integer :: log_n, n_iter
+   integer :: log_n_words, n_words
 
    integer :: c0, c1, crate
    real :: time_pflogger, time_raw
@@ -13,30 +14,40 @@ program main
    call initialize()
    call config()
 
-   print*, '  n          T(pflogger)       T(raw)      ratio'
+   print*, '  n_iter   n_words    T(pflogger)       T(raw)      ratio'
 
-   do log_n = 3, 5
-      n = 10** log_n
-   
-      call system_clock(c0, crate)
-      call bench_pflogger(n)
-      call system_clock(c1)
-      time_pflogger = real(c1 - c0) / crate
+   do log_n = 5, 5
+      n_iter = 10** log_n
 
-      call system_clock(c0)
-      call bench_raw(n)
-      call system_clock(c1)
-      time_raw = real(c1 - c0) / crate
+      do log_n_words = 1, 4
+         n_words = 2**(log_n_words-1)
+         call system_clock(c0, crate)
+         call bench_pflogger(n_iter, n_words)
+         call system_clock(c1)
+         time_pflogger = real(c1 - c0) / crate
 
-      print*,n, time_pflogger, time_raw, time_pflogger / time_raw
+         call system_clock(c0)
+         call bench_raw(n_iter, n_words)
+         call system_clock(c1)
+         time_raw = real(c1 - c0) / crate
 
-      ! clean up files
-      open(file='foo.txt',newunit=unit)
-      close(unit, status='delete')
+         print*,n_iter, n_words, time_pflogger, time_raw, time_pflogger / time_raw
 
-      open(file='foo_raw.txt',newunit=unit)
-      close(unit, status='delete')
+         ! clean up files
+         open(file='foo.txt',newunit=unit)
+         close(unit, status='delete')
 
+         open(file='foo_raw.txt',newunit=unit)
+         close(unit, status='delete')
+
+         block
+           integer :: i
+           do i = 1, size(c_all)
+              print*,i,  real(c_all(i))/crate
+           end do
+           c_all = 0
+         end block
+      end do
    end do
 
    call finalize()
@@ -59,41 +70,43 @@ contains
 
    end subroutine config
 
-   subroutine bench_pflogger(n)
-      integer, intent(in) :: n
+   subroutine bench_pflogger(n_iter, n_words)
+      integer, intent(in) :: n_iter
+      integer, intent(in) :: n_words
       type (Logger), pointer :: my_logger
       integer :: i
 
-      real :: x
+      real :: x(n_words)
       
       my_logger => logging%getLogger('A')
-      do i = 1, n
+      do i = 1, n_iter
          call do_work(i, x)
-         call my_logger%INFO('hello %i8 %f8.4', i, x)
+         call my_logger%INFO('hello %i8 %10(f8.4,1x)', i, wrapArray(x))
       end do
 
    end subroutine bench_pflogger
 
 
-   subroutine bench_raw(n)
-      integer, intent(in) :: n
+   subroutine bench_raw(n_iter, n_words)
+      integer, intent(in) :: n_iter
+      integer, intent(in) :: n_words
 
       integer :: i
       integer :: unit
-      real :: x
+      real :: x(n_words)
 
       open(file='foo_raw.txt', newunit=unit, status='new', form='formatted')
 
-      do i = 1, n
+      do i = 1, n_iter
          call do_work(i, x)
-         write(unit,'(a, i8, 1x, f8.4)')'hello ', i, x
+         write(unit,'(a, i8, 1x, 10(f8.4,1x))')'hello ', i, x
       end do
 
    end subroutine bench_raw
 
    subroutine do_work(i, x)
       integer, intent(in) :: i
-      real, intent(out) :: x
+      real, intent(out) :: x(:)
 
       call random_number(x)
 
