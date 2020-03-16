9eb6e00b70a574b75855e101d4b46a01fcec3914
Matthew Thompson <matthew.thompson@nasa.gov>
2020-03-16 15:43:35 -0400

    Updates for gfortran
    Test building on macOS with gfortran showed two issues:
    
    1. Set `-ffree-line-length-none` as apparently 255 was too short
    2. Fix for installing a `.fh` file

--- cmake/GNU.cmake
+++ cmake/GNU.cmake
@@ -1,5 +1,5 @@
 set (FPP_FLAG -cpp)
-set (common_flags "${FPP_FLAG} -ffree-line-length-255")
+set (common_flags "${FPP_FLAG} -ffree-line-length-none")
 
 set (CMAKE_Fortran_FLAGS_RELEASE "${common_flags} -O3")
 set (CMAKE_Fortran_FLAGS_DEBUG "${common_flags} -O0 -g -fbacktrace -fcheck=pointer -fcheck=mem \
