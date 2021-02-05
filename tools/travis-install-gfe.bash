8f062a2b679f6003d96671400df3f1acfd1fadc7
Matthew Thompson <matthew.thompson@nasa.gov>
2021-02-05 10:46:24 -0500

    Fix travis

--- tools/travis-install-gfe.bash
+++ tools/travis-install-gfe.bash
@@ -9,20 +9,14 @@ mkdir -p ${GFE_DIR}
 GFE_INSTALL_DIR=${HOME}/Software/GFE
 mkdir -p ${GFE_INSTALL_DIR}
 
-to_build=(gFTL gFTL-shared fArgParse yaFyaml pFUnit)
+to_build=(pFUnit yaFyaml)
 for repo in "${to_build[@]}"
 do
-   echo "Running build for ${repo}"
    cd ${GFE_DIR}
    git clone https://github.com/Goddard-Fortran-Ecosystem/${repo}.git
    cd ${GFE_DIR}/${repo}
-   if [[ "${repo}" != "gFTL" ]]
-   then
-      git checkout bugfix/mathomp4/build_submodule_fix
-   fi
    mkdir build && cd build
    cmake .. -DCMAKE_INSTALL_PREFIX=${GFE_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${GFE_INSTALL_DIR}
    make -j$(nproc) install
-   echo "Completed build for ${repo}"
 done
 
